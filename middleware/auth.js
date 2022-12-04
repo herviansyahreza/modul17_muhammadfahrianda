a620f69778c4b3ef0668dc08589b3f5fa5074934
herviansyahreza <82496908+herviansyahreza@users.noreply.github.com>
2022-12-04 22:46:23 +0700

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,19 +1,21 @@
 const jwt = require('jsonwebtoken');
-
-
 SECRET = process.env.SECRET
+
 const Auth = {
     verifyToken(req, res, next){
-      token = req.cookies["JSON Web Token"]
+        // token = req.cookies['JWT']
+        // console.log(token);
+        token = req.body.token
         if (token) {
-            // 12. Lalukan jwt verify 
-          const verified = jwt.verify(token, SECRET)
-          if(verified){
-            console.log("Verifikasi anda sukses");
-            return next();
-          } else {
-            return res.status(401).send(error);
-          }
+            // 12. Lakukan jwt verify 
+            const verified = jwt.verify(token, SECRET);
+            if(verified){
+              console.log("You have been Verified");
+              return next();
+            }else{
+              return res.status(401).send(error);
+            }
+
         } else {
           res.status(403).send({message: 'Youre not authenticated, please login first'})
             console.log('Youre not authenticated');
